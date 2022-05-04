38b329409e55262839d1ca828b79b03dc51c8d0e
Esteban Rodriguez <erodriguezg7@gmail.com>
2022-05-04 02:38:36 -0500

    Agregando prueba jest para ExplorerController.js

---
+++ test/controllers/ExplorerController.test.js
@@ -0,0 +1,28 @@
+const ExplorerController = require("../../lib/controllers/ExplorerController");
+
+describe("Test for ExplorerController", ()=>{
+
+    test("Prueba 1: getExplorers by Mission", ()=>{
+
+        const mission = "node";
+        const explorersInMission = ExplorerController.getExplorersByMission(mission);
+
+        expect(explorersInMission.length).toBe(10);
+    });
+
+    test("Prueba 2: getAmount of explorers by Mission", ()=>{
+
+        const mission = "node";
+        const amountOfExplorers = ExplorerController.getExplorersAmountByMission(mission);
+
+        expect(amountOfExplorers).toBe(10);
+    });
+
+    test("Prueba 3: getUsernames of explorers by Mission", ()=>{
+
+        const mission = "java";
+        const usernames = ExplorerController.getExplorersUsernamesByMission(mission);
+
+        expect(usernames.length).toBe(5);
+    });
+});
