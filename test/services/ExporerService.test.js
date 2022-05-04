58763de80d73c46232e838a2bd19a0c3f80cae1b
Esteban Rodriguez <erodriguezg7@gmail.com>
2022-05-04 00:31:36 -0500

    Paso 21: Agreando Linter y ejecutando Linter Fix

--- test/services/ExporerService.test.js
+++ test/services/ExporerService.test.js
@@ -1,38 +1,38 @@
-const ExplorerService = require('./../../lib/services/ExplorerService')
+const ExplorerService = require("./../../lib/services/ExplorerService");
 
 describe("Test for ExplorerService",()=>{
 
     test("Requerimiento 1: Calcular el total de explorers en una mision", ()=>{
 
-        const explorers = [{name: "Exp 1", mission: "node"},{name:"Exp 2", mission:"node"},{name:"Exp 3", mission:"java"},{name:"Exp 4", mission:"node"}]
+        const explorers = [{name: "Exp 1", mission: "node"},{name:"Exp 2", mission:"node"},{name:"Exp 3", mission:"java"},{name:"Exp 4", mission:"node"}];
 
-        const AmountOfExplorersInNodeMission = ExplorerService.getAmountOfExplorersByMission(explorers,"node")
+        const AmountOfExplorersInNodeMission = ExplorerService.getAmountOfExplorersByMission(explorers,"node");
 
-        expect(AmountOfExplorersInNodeMission).toBe(3)
+        expect(AmountOfExplorersInNodeMission).toBe(3);
     });
 
     test("Requerimiento 2: Obtener la lista de explorers en una mission", ()=>{
 
-        const explorers = [{name: "Exp 1", mission: "node"},{name:"Exp 2", mission:"java"},{name:"Exp 3", mission:"java"},{name:"Exp 4", mission:"node"}]
+        const explorers = [{name: "Exp 1", mission: "node"},{name:"Exp 2", mission:"java"},{name:"Exp 3", mission:"java"},{name:"Exp 4", mission:"node"}];
 
         const explorersInNodeMission = ExplorerService.filterByMission(explorers, "node");
 
-        expect(explorersInNodeMission.length).toBe(2)
+        expect(explorersInNodeMission.length).toBe(2);
     });
 
     
     test("Requerimiento 3: Obtener los username de los explorers", ()=>{
 
-        const explorers = [{githubUsername: "erodriguezg27", mission: "node"}]
+        const explorers = [{githubUsername: "erodriguezg27", mission: "node"}];
 
         const usernames = ExplorerService.getExplorersUsernamesByMission(explorers,"node");
 
         expect(usernames[0]).toBe("erodriguezg27");
 
-    })
+    });
 
     
 
     
 
-})
+});
--- test/utils/Reader.test.js
+++ test/utils/Reader.test.js
@@ -1,11 +1,11 @@
-const Reader = require('./../../lib/utils/Reader')
+const Reader = require("./../../lib/utils/Reader");
 
 describe("Test for Reader",()=>{
 
     test("Requerimiento 1: Leer un archivo JSON dado un path", ()=>{
 
-        const explorers = Reader.readJsonFIle("./datos.json")
+        const explorers = Reader.readJsonFIle("./datos.json");
 
-        expect(explorers.length).toBe(3)
-    })
-})
+        expect(explorers.length).toBe(3);
+    });
+});
